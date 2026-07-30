82caa40b66ee9f3e8a1e5021a17a2f6b335d9f42
lfegomes <luisgomes.tech@gmail.com>
2026-07-29 21:02:47 -0300

    chore: initialize Dart console application template

---
+++ dataset/bin/dataset.dart
@@ -0,0 +1,5 @@
+import 'package:dataset/dataset.dart' as dataset;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dataset.calculate()}!');
+}
---
+++ dataset/test/dataset_test.dart
@@ -0,0 +1,8 @@
+import 'package:dataset/dataset.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
